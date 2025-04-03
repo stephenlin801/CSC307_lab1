c86225097736b1503c90cfda2054a1c70707f40f
stephenlin801 <145500043+stephenlin801@users.noreply.github.com>
2025-04-03 14:38:14 -0700

    second commit before moving on to form submit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -1,29 +1,12 @@
 // src/MyApp.jsx
 import React, {useState}  from "react";
 import Table from "./Table";
-
+import Form from "./Form";
 
 
 function MyApp() {
 
-    const [characters, setCharacters] = useState([
-        {
-          name: "Charlie",
-          job: "Janitor"
-        },
-        {
-          name: "Mac",
-          job: "Bouncer"
-        },
-        {
-          name: "Dee",
-          job: "Aspring actress"
-        },
-        {
-          name: "Dennis",
-          job: "Bartender"
-        }
-      ]);
+    const [characters, setCharacters] = useState([]);
 
       function removeOneCharacter(index) {
         const updated = characters.filter((character, i) => {
@@ -37,6 +20,7 @@ function MyApp() {
     <div className="container">
       <Table characterData={characters}
       removeCharacter={removeOneCharacter}/>
+      <Form />
     </div>
   );
 }
