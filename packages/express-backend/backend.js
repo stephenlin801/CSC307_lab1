861c9325aa14cc1b2192f6118da89803f022ee89
stephenlin801 <145500043+stephenlin801@users.noreply.github.com>
2025-04-07 18:28:38 -0700

    post for backend

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -60,15 +60,26 @@ if (name != undefined) {
 const findUserById = (id) =>
     users["users_list"].find((user) => user["id"] === id);
   
-  app.get("/users/:id", (req, res) => {
-    const id = req.params["id"]; //or req.params.id
-    let result = findUserById(id);
-    if (result === undefined) {
-      res.status(404).send("Resource not found.");
-    } else {
-      res.send(result);
-    }
-  });
+app.get("/users/:id", (req, res) => {
+const id = req.params["id"]; //or req.params.id
+let result = findUserById(id);
+if (result === undefined) {
+    res.status(404).send("Resource not found.");
+} else {
+    res.send(result);
+}
+});
+
+const addUser = (user) => {
+users["users_list"].push(user);
+return user;
+};
+
+app.post("/users", (req, res) => {
+const userToAdd = req.body;
+addUser(userToAdd);
+res.send();
+});
 
 app.listen(port, () => {
   console.log(
