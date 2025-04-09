88ab6ca21f3934936164a55156fb3e10a3a00ff9
stephenlin801 <145500043+stephenlin801@users.noreply.github.com>
2025-04-09 10:46:08 -0700

    finished extending backend functions

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -34,27 +34,54 @@ const users = {
     ]
   };
 
-const findUserByName = (name) => {
-return users["users_list"].filter(
-    (user) => user["name"] === name
-);
-};
-
 app.use(express.json());
 
 app.get("/", (req, res) => {
   res.send("Hello Again and Again World!");
 });
 
-app.get("/users", (req, res) => {
+
+const findUserByName = (name) => {
+    return users["users_list"].filter(
+        (user) => user["name"] === name
+    );
+    };
+
+const findUserByJob = (job) => {
+    return users["users_list"].filter ((user) => user["job"] === job);
+};
+
+const findUserByJobandName = (job, name) => {
+    let result = users["users_list"].filter((user) => user["name"] === name);
+    return result.filter ((user) => user.job === job);
+};
+
+app.get("/users", (req, res) => { // need to get this extended better
 const name = req.query.name;
-if (name != undefined) {
-    let result = findUserByName(name);
-    result = { users_list: result };
-    res.send(result);
-} else {
-    res.send(users);
+const job = req.query.job;
+
+if (name != undefined && job != undefined) { // there is probably a better way to do this
+    let bothresult = findUserByJobandName(job, name);
+    bothresult = { users_list: bothresult };
+    res.send(bothresult)
 }
+
+else if (name != undefined) {
+    let nameresult = findUserByName(name);
+    nameresult = { users_list: nameresult };
+    res.send(nameresult)
+}
+
+else if (job != undefined) {
+    let jobresult = findUserByJob(job);
+    jobresult = { users_list: jobresult}
+    res.send(jobresult)
+}
+
+else{
+    res.send(users)
+}
+
 });
 
 const findUserById = (id) =>
@@ -80,6 +107,25 @@ const userToAdd = req.body;
 addUser(userToAdd);
 res.send();
 });
+
+
+const deleteUserById = (id) => {
+    users["users_list"] = users["users_list"].filter((user) => user.id !== id);
+};
+
+app.delete("/users/:id", (req, res) => { // this is insecure but also fine, however to make it more secure have the id in the body and have it read it that way
+const id = req.params.id;
+let result = findUserById(id);
+if (result === undefined) {
+    res.status(404).send("Resource not found.");
+}
+else{
+    deleteUserById(id);
+    res.end();
+}
+
+});
+
 
 app.listen(port, () => {
   console.log(
